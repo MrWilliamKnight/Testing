dc5baa8cfab1551f17327c48babeecd3ae1100fd
MrWilliamKnight <gmo.sepulveda.a@gmail.com>
2021-02-15 18:30:58 -0300

    slideContent

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
